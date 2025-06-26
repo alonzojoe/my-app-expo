b71321c40a11a081c7b0e2fcd1da9719d9b12405
alonzojoe <alonzojoenell2k15@gmail.com>
2025-06-26 09:54:14 +0800

    style Tabs Screen and add package expo/vector-icons

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,15 +1,31 @@
-import { StyleSheet, StatusBar, Stack } from "react-native";
+import { StyleSheet, StatusBar, Stack, useColorScheme } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { Colors } from "../../constants/Colors";
 
 const DashboardLayout = () => {
+  const colorScheme = useColorScheme();
+
+  const theme = Colors[colorScheme] ?? Colors.light;
+
   return (
     <>
       <Tabs
         screenOptions={{
           headerTitle: "",
+          tabBarStyle: {
+            backgroundColor: theme.navBackground,
+            paddingTop: 10,
+            height: 90,
+          },
+          tabBarActiveTintColor: theme.iconColourFocused,
+          tabBarInactiveTintColor: theme.iconColour,
         }}
-      />
+      >
+        <Tabs.Screen name="create" options={{ title: "Create" }} />
+        <Tabs.Screen name="todos" options={{ title: "Todos" }} />
+        <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+      </Tabs>
     </>
   );
 };
