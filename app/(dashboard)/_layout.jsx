c1612e8a9f161c08f542fd9c412691e9fecb3e76
alonzojoe <alonzojoenell2k15@gmail.com>
2025-06-26 10:17:04 +0800

    styling tabs navigation DashboardLayout add icons and focus state

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -2,6 +2,8 @@ import { StyleSheet, StatusBar, Stack, useColorScheme } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { Colors } from "../../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { name } from "./../../node_modules/ajv/dist/compile/codegen/code";
 
 const DashboardLayout = () => {
   const colorScheme = useColorScheme();
@@ -22,9 +24,45 @@ const DashboardLayout = () => {
           tabBarInactiveTintColor: theme.iconColour,
         }}
       >
-        <Tabs.Screen name="create" options={{ title: "Create" }} />
-        <Tabs.Screen name="todos" options={{ title: "Todos" }} />
-        <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                size={24}
+                name={focused ? "create" : "create-outline"}
+                color={focused ? theme.iconColourFocused : theme.iconColour}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="todos"
+          options={{
+            title: "Todos",
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                size={24}
+                name={focused ? "list" : "list-outline"}
+                color={focused ? theme.iconColourFocused : theme.iconColour}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                size={24}
+                name={focused ? "person" : "person-outline"}
+                color={focused ? theme.iconColourFocused : theme.iconColour}
+              />
+            ),
+          }}
+        />
       </Tabs>
     </>
   );
