5cf5c4176bef9962f86aab7674b9688f13d93e21
alonzojoe <alonzojoenell2k15@gmail.com>
2025-06-27 16:31:07 +0800

    home component services items, responsive + scroll view component

--- app/(dashboard)/home.jsx
+++ app/(dashboard)/home.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { Text as PaperText } from "react-native-paper";
 import React from "react";
 import SafeView from "../../components/SafeView";
@@ -6,6 +6,8 @@ import Header from "../../components/Header";
 import { Avatar, Button, Card } from "react-native-paper";
 import Banner from "../../assets/banner.jpg";
 import Subtitle from "../../components/Subtitle";
+
+const ITEMS = Array.from({ length: 6 }).map((_, index) => index + 1);
 
 const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 const Home = () => {
@@ -17,7 +19,34 @@ const Home = () => {
         <Card.Cover source={Banner} />
       </View>
 
-      <Subtitle label="Services" />
+      <Subtitle
+        label={`Services: ${ITEMS.length}`}
+        style={{ marginVertical: 15 }}
+      />
+      <ScrollView
+        contentContainerStyle={{ paddingHorizontal: 16, flexGrow: 1 }}
+      >
+        <View style={styles.serviceContainer}>
+          {ITEMS.map((i) => (
+            <Card key={i} style={styles.card}>
+              {/* <Card.Title
+                title="Card Title"
+                subtitle="Card Subtitle"
+                left={LeftContent}
+              /> */}
+              {/* <Card.Content>
+                <PaperText variant="titleLarge">Card title</PaperText>
+                <PaperText variant="bodyMedium">Card content</PaperText>
+              </Card.Content> */}
+              <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+              {/* <Card.Actions>
+                <Button>Cancel</Button>
+                <Button>Ok</Button>
+              </Card.Actions> */}
+            </Card>
+          ))}
+        </View>
+      </ScrollView>
     </SafeView>
   );
 };
@@ -32,6 +61,17 @@ const styles = StyleSheet.create({
     // backgroundColor: "#fff",
     // alignItems: "center",
     // justifyContent: "center",
+  },
+  serviceContainer: {
+    flexDirection: "row",
+    gap: 10,
+    flexWrap: "wrap",
+    justifyContent: "space-between", // or 'center' or 'space-around'
+    paddingHorizontal: 0,
+    paddingBottom: 100,
+  },
+  card: {
+    width: "48%",
   },
   title: {
     fontWeight: "bold",
