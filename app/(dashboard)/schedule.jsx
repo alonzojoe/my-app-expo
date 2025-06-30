c9068f34eb941ecb5683d0edca3c605212e82ef8
alonzojoe <alonzojoenell2k15@gmail.com>
2025-06-30 11:50:16 +0800

    make AppointmentItem as dynamic into separate component accept props service and appointment and integrate in dummy data of Appointments

--- app/(dashboard)/schedule.jsx
+++ app/(dashboard)/schedule.jsx
@@ -2,51 +2,16 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import SafeView from "../../components/SafeView";
 import Header from "../../components/Header";
-import {
-  Text as PaperText,
-  List,
-  Divider,
-  IconButton,
-} from "react-native-paper";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-
+import ListItem from "../../components/Appointment/ListItem";
+import { APPOINTMENTS } from "../../constants/Appointments";
 const Schedule = () => {
   return (
     <SafeView safe={true}>
       <Header />
       <View style={styles.container}>
-        <List.Item
-          title="Pediatrics"
-          description="July 21, 2025 10:00 AM"
-          left={(props) => (
-            <View
-              style={{
-                backgroundColor: "#e0e0e0",
-                borderRadius: 24,
-                width: 40,
-                height: 40,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <IconButton
-                icon="calendar"
-                size={25}
-                iconColor="black"
-                style={{ margin: 0 }}
-              />
-            </View>
-          )}
-          right={(props) => (
-            <FontAwesome
-              onPress={() => console.log("cancel")}
-              name="times"
-              size={24}
-              color="#DD3353"
-            />
-          )}
-        />
-        <Divider />
+        {APPOINTMENTS.map((a) => (
+          <ListItem service={a.service} appointment={a.appointment} />
+        ))}
       </View>
     </SafeView>
   );
--- components/Appointment/ListItem.jsx
+++ components/Appointment/ListItem.jsx
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View } from "react-native";
+import { List, Divider, IconButton } from "react-native-paper";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-const ListItem = () => {
+const ListItem = ({ service, appointment }) => {
   return (
-    <View>
-      <Text>ListItem</Text>
-    </View>
-  )
-}
+    <>
+      <List.Item
+        title={service}
+        description={appointment}
+        left={(props) => (
+          <View
+            style={{
+              backgroundColor: "#e0e0e0",
+              borderRadius: 24,
+              width: 40,
+              height: 40,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <IconButton
+              icon="calendar"
+              size={25}
+              iconColor="black"
+              style={{ margin: 0 }}
+            />
+          </View>
+        )}
+        right={(props) => (
+          <FontAwesome
+            onPress={() => console.log("cancel")}
+            name="times"
+            size={24}
+            color="#DD3353"
+          />
+        )}
+      />
+      <Divider />
+    </>
+  );
+};
 
-export default ListItem
+export default ListItem;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
