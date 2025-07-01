d7a85cdff439bbc57bbc1b9b6d383ed62f121709
alonzojoe <alonzojoenell2k15@gmail.com>
2025-07-01 15:58:35 +0800

    create profile component jsx wrap it with SafeView reusable component

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -1,13 +1,46 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import SafeView from "../../components/SafeView";
+import {
+  Avatar,
+  Card,
+  Text as PaperText,
+  IconButton,
+  Button,
+  Appbar,
+} from "react-native-paper";
 import { StatusBar } from "expo-status-bar";
+const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 const Profile = () => {
   return (
     <>
-      <StatusBar style="auto" />
-      <SafeView safe={true} style={styles.container}>
-        <Text style={styles.title}>Profile</Text>
+      <SafeView safe={true}>
+        {/* <Text style={styles.title}>Profile</Text> */}
+
+        <View style={styles.container}>
+          <Appbar.Header
+            style={{
+              backgroundColor: "#2F4F7F",
+              marginTop: 0,
+            }}
+          >
+            <Appbar.BackAction onPress={() => {}} />
+            <Appbar.Content title="Title" />
+            <Appbar.Action icon="calendar" onPress={() => {}} />
+            <Appbar.Action icon="magnify" onPress={() => {}} />
+          </Appbar.Header>
+          <Card>
+            <Card.Title
+              title="Card Title"
+              subtitle="Card Subtitle"
+              left={LeftContent}
+            />
+            <Card.Content>
+              <PaperText variant="titleLarge">Card title</PaperText>
+              <PaperText variant="bodyMedium">Card content</PaperText>
+            </Card.Content>
+          </Card>
+        </View>
       </SafeView>
     </>
   );
@@ -17,9 +50,12 @@ export default Profile;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
+    paddingHorizontal: 16,
     // justifyContent: "center",
+  },
+  fluid: {
+    display: "flex",
+    flexDirection: "row",
   },
   title: {
     fontWeight: "bold",
