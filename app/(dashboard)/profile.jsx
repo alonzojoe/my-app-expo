d70845b7dc5a6bcc9d670bfde95e95dfe9f1ae30
alonzojoe <alonzojoenell2k15@gmail.com>
2025-07-02 11:45:39 +0800

    profile section add header card displaying name and account type, hospital number and phone number

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -1,17 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import SafeView from "../../components/SafeView";
-import { Avatar, Card, Text as PaperText } from "react-native-paper";
+import {
+  Avatar,
+  Card,
+  List,
+  Divider,
+  Text as PaperText,
+} from "react-native-paper";
 
 import { PROFILEITEMS } from "./../../constants/ProfileItems";
 import ProfileItem from "./../../components/Profile/ProfileItem";
+import ProfileImg from "../../assets/image/Default_pfp.jpg";
 const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 const Profile = () => {
   return (
     <>
       <SafeView safe={true}>
         <View style={styles.container}>
-          <Card>
+          {/* <Card
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <Card.Title
               title="Card Title"
               subtitle="Card Subtitle"
@@ -21,8 +35,43 @@ const Profile = () => {
               <PaperText variant="titleLarge">Card title</PaperText>
               <PaperText variant="bodyMedium">Card content</PaperText>
             </Card.Content>
-          </Card>
-          <View style={{ paddingHorizontal: 10 }}>
+          </Card> */}
+          <View
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 10,
+            }}
+          >
+            <Avatar.Image size={90} source={ProfileImg} />
+            <View style={{ alignItems: "center" }}>
+              <PaperText
+                variant="headlineSmall"
+                style={{ fontWeight: "bold", color: "#091D63" }}
+              >
+                Joe Alonzo
+              </PaperText>
+              <PaperText
+                variant="bodyMedium"
+                style={{ fontWeight: "bold", color: "#6E7AA3" }}
+              >
+                Patient
+              </PaperText>
+            </View>
+          </View>
+          <View>
+            <List.Item
+              title="000590599"
+              left={(props) => <List.Icon {...props} icon="card" />}
+            />
+            <List.Item
+              title="095632145896"
+              left={(props) => <List.Icon {...props} icon="phone" />}
+            />
+          </View>
+          <View style={{ paddingHorizontal: 10, marginTop: 10 }}>
+            <Divider />
             {PROFILEITEMS.map((prof) => (
               <ProfileItem
                 key={prof.id}
