179831735ddd2bd5b58fb8985f6438586ca7a8b7
alonzojoe <alonzojoenell2k15@gmail.com>
2025-07-09 10:32:45 +0800

    update Medical Records TransactionItem add new onView prop for viewing of details of Patient's TransactionNumber

--- app/medical.jsx
+++ app/medical.jsx
@@ -1,16 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useState, useMemo } from "react";
 import SafeView from "../components/SafeView";
-import { Searchbar } from "react-native-paper";
+import {
+  Searchbar,
+  Portal,
+  Modal,
+  Text as PaperText,
+} from "react-native-paper";
 import TransactionItem from "../components/Transactions/TransactionItem";
 import { ScrollView } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { MEDICAL_RECORDS } from "../constants/global";
 import useDebounce from "./../hooks/useDebounce";
+import useToggle from "../hooks/useToggle";
 const Medical = () => {
   const { bottom } = useSafeAreaInsets;
   const [searchQuery, setSearchQuery] = useState("");
   const debounceValue = useDebounce(searchQuery);
+  const [show, toggleShow] = useToggle(false);
 
   const searchDebounce = useDebounce(searchQuery);
 
@@ -44,6 +51,7 @@ const Medical = () => {
         >
           {filteredRecords.map((medical) => (
             <TransactionItem
+              onView={() => toggleShow(true)}
               key={medical.id}
               transaction={medical.transactionNo}
               transactionDate={medical.transactionDate}
@@ -51,6 +59,57 @@ const Medical = () => {
           ))}
         </View>
       </ScrollView>
+      <Portal>
+        <Modal
+          visible={show}
+          onDismiss={() => toggleShow(false)}
+          contentContainerStyle={styles.modalContainer}
+        >
+          <>
+            <View
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                marginBottom: 10,
+              }}
+            >
+              <PaperText variant="titleSmall">FAQ Title</PaperText>
+            </View>
+            <ScrollView>
+              <PaperText>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Blanditiis, ducimus quo animi sunt enim aperiam! Totam fugit
+                illum voluptatem ullam, iusto culpa sit necessitatibus earum
+                ipsum nulla cum ipsam velit. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Blanditiis, ducimus quo animi sunt
+                enim aperiam! Totam fugit illum voluptatem ullam, iusto culpa
+                sit necessitatibus earum ipsum nulla cum ipsam velit. Lorem
+                ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis,
+                ducimus quo animi sunt enim aperiam! Totam fugit illum
+                voluptatem ullam, iusto culpa sit necessitatibus earum ipsum
+                nulla cum ipsam velit.
+              </PaperText>
+            </ScrollView>
+            <View style={{ marginVertical: 10 }} />
+            <View
+              style={{
+                flexDirection: "row",
+                display: "flex",
+                alignContent: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              <PaperText
+                style={{ color: "#DD3254", fontWeight: "bold" }}
+                onPress={() => toggleShow(false)}
+              >
+                Close
+              </PaperText>
+            </View>
+          </>
+        </Modal>
+      </Portal>
     </SafeView>
   );
 };
@@ -61,5 +120,11 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 10,
     paddingHorizontal: 16,
+  },
+  modalContainer: {
+    borderRadius: 5,
+    marginHorizontal: 20,
+    backgroundColor: "white",
+    padding: 20,
   },
 });
