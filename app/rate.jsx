a4490cb2b5d8033c74896ac9087533154f531c0a
alonzojoe <alonzojoenell2k15@gmail.com>
2025-07-11 11:39:54 +0800

    implement ratings star selection using TouchableOpacity from react native, + ratings state by default set it as 5

--- app/rate.jsx
+++ app/rate.jsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 import SafeView from "../components/SafeView";
 import { Card, Text as PaperText, Button, TextInput } from "react-native-paper";
 import { ScrollView } from "react-native";
@@ -8,50 +8,59 @@ import { Colors } from "../constants/Colors";
 import AntDesign from "@expo/vector-icons/AntDesign";
 const OnlineAppointment = () => {
   const { bottom } = useSafeAreaInsets();
+  const [ratings, setRatings] = useState(5);
   const color = Colors["light"];
 
   return (
     <SafeView>
       <ScrollView style={{ marginBottom: bottom }}>
-        <View style={styles.container}>
-          <PaperText
-            variant="titleMedium"
-            style={{ fontWeight: "400", fontSize: 20, textAlign: "center" }}
-          >
-            Share us your experience!
-          </PaperText>
-
-          <View
-            style={{
-              marginTop: 15,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "row",
-              gap: 5,
-            }}
-          >
-            <AntDesign name="star" size={30} color="#FFCC00" />
-            <AntDesign name="star" size={30} color="#FFCC00" />
-            <AntDesign name="star" size={30} color="#FFCC00" />
-            <AntDesign name="star" size={30} color="#C0C0C0" />
-            <AntDesign name="star" size={30} color="#C0C0C0" />
-          </View>
-          <View style={{ paddingHorizontal: 15, marginTop: 15 }} mult>
-            <TextInput multiline={true} label="Feedback" />
-          </View>
-          <View style={styles.textGroup}>
-            <Button
-              mode="contained"
-              onPress={() => {
-                console.log("test");
-              }}
-              style={styles.btn}
+        <Card
+          style={{ marginHorizontal: 16, marginTop: 20, marginBottom: bottom }}
+        >
+          <View style={styles.container}>
+            <PaperText
+              variant="titleMedium"
+              style={{ fontWeight: "400", fontSize: 20, textAlign: "center" }}
             >
-              Submit
-            </Button>
+              Share us your experience!
+            </PaperText>
+
+            <View
+              style={{
+                marginTop: 15,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "row",
+                gap: 5,
+              }}
+            >
+              {[1, 2, 3, 4, 5].map((star) => (
+                <TouchableOpacity key={star} onPress={() => setRatings(star)}>
+                  <AntDesign
+                    name="star"
+                    size={30}
+                    color={star <= ratings ? "#FFCC00" : "#C0C0C0"}
+                  />
+                </TouchableOpacity>
+              ))}
+            </View>
+            <View style={{ paddingHorizontal: 15, marginTop: 20 }} mult>
+              <TextInput multiline={true} label="Feedback" />
+            </View>
+            <View style={styles.textGroup}>
+              <Button
+                mode="contained"
+                onPress={() => {
+                  console.log("test");
+                }}
+                style={styles.btn}
+              >
+                Submit
+              </Button>
+            </View>
           </View>
-        </View>
+        </Card>
       </ScrollView>
     </SafeView>
   );
@@ -72,6 +81,7 @@ const styles = StyleSheet.create({
     gap: 5,
   },
   textGroup: {
+    marginBottom: 15,
     paddingHorizontal: 15,
     marginTop: 15,
     alignItems: "center",
