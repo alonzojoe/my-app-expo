cc5efb4c61b28ff160602840ffdebde5b5f308ce
alonzojoe <alonzojoenell2k15@gmail.com>
2025-07-17 09:38:03 +0800

    create new zod form validation verifySchema for VerificationForm

---
+++ schema/schema.js
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const verifySchema = z.object({
+  patientno: z
+    .string()
+    .min(5)
+    .nonempty({ message: "Hospital No. is required!" }),
+  lastname: z.string().trim().nonempty({ message: "Last Name is required" }),
+  lastname: z
+    .string()
+    .trim()
+    .min(10)
+    .nonempty({ message: "Last Name is required" }),
+});
