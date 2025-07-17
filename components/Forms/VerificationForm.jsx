d474becff4f4952a04dac4cde6792cbba33c31a1
alonzojoe <alonzojoenell2k15@gmail.com>
2025-07-17 10:23:13 +0800

    implement zod + useForm in VerificationForm component trigger the validation upon first trigger of Verify button

--- components/Forms/VerificationForm.jsx
+++ components/Forms/VerificationForm.jsx
@@ -1,11 +1,6 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import React, { useState } from "react";
-import {
-  Text as PaperText,
-  TextInput,
-  HelperText,
-  Button,
-} from "react-native-paper";
+import { Text as PaperText, TextInput, Button } from "react-native-paper";
 import QRScanner from "../QR/QRScanner";
 import useToggle from "../../hooks/useToggle";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
@@ -13,7 +8,7 @@ import MaskInput, { Masks } from "react-native-mask-input";
 import Spacer from "../Spacer";
 import ErrorMessage from "../Global/ErrorMessage";
 import { verifySchema } from "../../schema/schema";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const defaultValues = {
@@ -23,8 +18,6 @@ const defaultValues = {
 };
 
 const VerificationForm = () => {
-  const [hospitalNo, setHospitalNo] = useState("");
-  const [phone, setPhone] = React.useState("");
   const [focused, setFocused] = useState(false);
   const [showQr, toggleShowQr] = useToggle(false);
 
@@ -38,121 +31,155 @@ const VerificationForm = () => {
   });
 
   const onSubmit = async (formValues) => {
-    console.log("Form submitted");
+    console.log("Form submitted", formValues);
   };
 
   return (
     <View>
-      <>
-        <View style={[styles.topHeaderItem, { marginBottom: 10 }]}>
-          <PaperText
-            variant="titleMedium"
-            style={{
-              color: "#001C63",
-              fontSize: 20,
-              fontWeight: "bold",
-              textAlign: "center",
-            }}
-          >
-            Information Verification
-          </PaperText>
-          <MaterialIcons name="verified" size={24} color="#017BFF" />
-        </View>
-      </>
-      <>
-        <View style={[styles.headerItem, { marginTop: 20 }]}>
-          <PaperText
-            variant="titleMedium"
-            style={{ color: "#001C63", fontWeight: "bold" }}
-          >
-            Hospital Number
-          </PaperText>
-        </View>
-        <TextInput
-          value={hospitalNo}
-          onChangeText={(text) => setHospitalNo(text)}
-          keyboardType="numeric"
-          type="number"
-          label=""
-          mode="outlined"
-          error={true}
-          style={styles.input}
-          right={
-            <TextInput.Icon
-              color={showQr ? `#DD3353` : `#095185`}
-              icon={showQr ? "close" : "qrcode"}
-              onPress={() =>
-                showQr ? toggleShowQr(false) : toggleShowQr(true)
-              }
-            />
-          }
-        />
-        <ErrorMessage>Hospital Number is required!</ErrorMessage>
-      </>
-      {showQr && (
-        <QRScanner onScan={setHospitalNo} onClose={() => toggleShowQr(false)} />
-      )}
-      <>
-        <View style={[styles.headerItem, { marginTop: 20 }]}>
-          <PaperText
-            variant="titleMedium"
-            style={{ color: "#001C63", fontWeight: "bold" }}
-          >
-            Last Name
-          </PaperText>
-        </View>
-        <TextInput
-          type="number"
-          label=""
-          mode="outlined"
-          style={styles.input}
-        />
-      </>
-      <>
-        <View style={[styles.headerItem, { marginTop: 20 }]}>
-          <PaperText
-            variant="titleMedium"
-            style={{ color: "#001C63", fontWeight: "bold" }}
-          >
-            Birthdate (MM/DD/YYYY)
-          </PaperText>
-        </View>
-        <MaskInput
-          value={phone}
-          onChangeText={(masked, unmasked) => {
-            setPhone(masked);
-            console.log(masked);
-            console.log(unmasked);
+      <View style={[styles.topHeaderItem, { marginBottom: 10 }]}>
+        <PaperText
+          variant="titleMedium"
+          style={{
+            color: "#001C63",
+            fontSize: 20,
+            fontWeight: "bold",
+            textAlign: "center",
           }}
-          mask={Masks.DATE_MMDDYYYY}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-          maxLength={10}
-          style={[
-            styles.input,
-            {
-              keyboardType: "numeric",
-              backgroundColor: "#FFF",
-              borderColor: focused ? "#001C63" : "#C1BDC3",
-              borderWidth: 2,
-              borderRadius: 5,
-              padding: 15,
-              fontSize: 16,
-            },
-          ]}
+        >
+          Information Verification
+        </PaperText>
+        <MaterialIcons name="verified" size={24} color="#017BFF" />
+      </View>
+
+      {/* Hospital Number */}
+      <View style={[styles.headerItem, { marginTop: 20 }]}>
+        <PaperText
+          variant="titleMedium"
+          style={{
+            color: errors.patientno ? "#B3271C" : "#001C63",
+            fontWeight: "bold",
+          }}
+        >
+          Hospital Number
+        </PaperText>
+      </View>
+      <Controller
+        control={control}
+        name="patientno"
+        render={({ field }) => (
+          <TextInput
+            {...field}
+            keyboardType="numeric"
+            label=""
+            mode="outlined"
+            error={!!errors.patientno}
+            style={styles.input}
+            right={
+              <TextInput.Icon
+                color={showQr ? `#DD3353` : `#095185`}
+                icon={showQr ? "close" : "qrcode"}
+                onPress={() => toggleShowQr(!showQr)}
+              />
+            }
+          />
+        )}
+      />
+      {errors.patientno && (
+        <ErrorMessage>{errors.patientno.message}</ErrorMessage>
+      )}
+      {showQr && (
+        <QRScanner
+          onScan={(value) => field.onChange(value)}
+          onClose={() => toggleShowQr(false)}
         />
-      </>
+      )}
+
+      {/* Last Name */}
+      <View style={[styles.headerItem, { marginTop: 20 }]}>
+        <PaperText
+          variant="titleMedium"
+          style={{
+            color: errors.lastname ? "#B3271C" : "#001C63",
+            fontWeight: "bold",
+          }}
+        >
+          Last Name
+        </PaperText>
+      </View>
+      <Controller
+        control={control}
+        name="lastname"
+        render={({ field }) => (
+          <TextInput
+            {...field}
+            label=""
+            mode="outlined"
+            error={!!errors.lastname}
+            style={styles.input}
+          />
+        )}
+      />
+      {errors.lastname && (
+        <ErrorMessage>{errors.lastname.message}</ErrorMessage>
+      )}
+
+      {/* Birthdate */}
+      <View style={[styles.headerItem, { marginTop: 20 }]}>
+        <PaperText
+          variant="titleMedium"
+          style={{
+            color: errors.birthdate ? "#B3271C" : "#001C63",
+            fontWeight: "bold",
+          }}
+        >
+          Birthdate (MM/DD/YYYY)
+        </PaperText>
+      </View>
+      <Controller
+        control={control}
+        name="birthdate"
+        render={({ field }) => (
+          <MaskInput
+            keyboardType="numeric"
+            value={field.value}
+            onChangeText={(masked, unmasked) => field.onChange(masked)}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+            mask={Masks.DATE_MMDDYYYY}
+            maxLength={10}
+            style={[
+              styles.input,
+              {
+                keyboardType: "numeric",
+                backgroundColor: "#FFF",
+                borderColor: errors.birthdate
+                  ? "#B3271C"
+                  : focused
+                  ? "#001C63"
+                  : "#C1BDC3",
+                borderWidth: 2,
+                borderRadius: 5,
+                padding: 15,
+                fontSize: 16,
+              },
+            ]}
+          />
+        )}
+      />
+      {errors.birthdate && (
+        <ErrorMessage>{errors.birthdate.message}</ErrorMessage>
+      )}
+
       <View style={styles.textGroup}>
         <Button
           mode="contained"
-          onPress={() => {
-            console.log("test");
-          }}
+          onPress={handleSubmit(onSubmit)}
           style={styles.btn}
         >
           Verify
         </Button>
       </View>
+
       <Spacer />
       <Spacer />
     </View>
@@ -162,19 +189,13 @@ const VerificationForm = () => {
 export default VerificationForm;
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 20,
-    paddingHorizontal: 20,
-  },
   topHeaderItem: {
-    display: "flex",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     gap: 5,
   },
   headerItem: {
-    display: "flex",
     flexDirection: "row",
     alignItems: "center",
     gap: 5,
--- schema/schema.js
+++ schema/schema.js
@@ -2,9 +2,11 @@ import { z } from "zod";
 
 export const verifySchema = z.object({
   patientno: z
-    .string()
-    .min(5)
-    .nonempty({ message: "Hospital No. is required!" }),
+    .number({
+      required_error: "Hospital No. is required!",
+      invalid_type_error: "Hospital No. must be a number",
+    })
+    .min(5, { message: "Hospital No. must be at least 5 digits" }),
   lastname: z.string().trim().nonempty({ message: "Last Name is required" }),
   birthdate: z
     .string()
