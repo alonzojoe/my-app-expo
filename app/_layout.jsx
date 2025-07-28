32f3abee1245cda059269532fa578de27f473bc7
alonzojoe <alonzojoenell2k15@gmail.com>
2025-07-28 13:52:32 +0800

    Wrap entire RootLayout to QueryClientProvider to use the tanstack react-query

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -8,7 +8,7 @@ import {
   MD3LightTheme as DefaultTheme,
   IconButton,
 } from "react-native-paper";
-
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { useRouter } from "expo-router";
 import store from "../store";
 
@@ -24,130 +24,132 @@ const RootLayout = () => {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
   const router = useRouter();
-
+  const queryClient = new QueryClient();
   console.log(theme);
   return (
     // <View style={{ flex: 1 }}>
-    <Provider store={store}>
-      <PaperProvider theme={appTheme}>
-        <StatusBar style="auto" />
-        <Stack
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: theme.navBackground,
-            },
-            headerTintColor: theme.title,
-            headerTitleAlign: "center",
-          }}
-        >
-          <Stack.Screen
-            name="index"
-            options={{
-              title: "Home",
-              headerShown: false,
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PaperProvider theme={appTheme}>
+          <StatusBar style="auto" />
+          <Stack
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: theme.navBackground,
+              },
+              headerTintColor: theme.title,
+              headerTitleAlign: "center",
             }}
-          />
-          <Stack.Screen
-            name="(auth)"
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="(dashboard)"
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="about"
-            options={{ title: "About Page", headerShown: true }}
-          />
-          {/* <Stack.Screen
+          >
+            <Stack.Screen
+              name="index"
+              options={{
+                title: "Home",
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="(auth)"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="(dashboard)"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="about"
+              options={{ title: "About Page", headerShown: true }}
+            />
+            {/* <Stack.Screen
           name="faqs"
           options={{ title: "FAQ", headerShown: true }}
         /> */}
-          <Stack.Screen
-            name="faqs"
-            options={{
-              title: "FAQ",
-              headerShown: true,
-              headerLeft: () => (
-                <IconButton
-                  icon="arrow-left"
-                  onPress={() => router.replace("/home")}
-                />
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="medical"
-            options={{
-              title: "Medical Records",
-              headerShown: true,
-              headerLeft: () => (
-                <IconButton
-                  icon="arrow-left"
-                  onPress={() => router.replace("/home")}
-                />
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="bluecard"
-            options={{
-              title: "My Virtual Blue Card",
-              headerShown: true,
-              headerLeft: () => (
-                <IconButton
-                  icon="arrow-left"
-                  onPress={() => router.replace("/profile")}
-                />
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="onlineappointment"
-            options={{
-              title: "Appointment Details",
-              headerShown: true,
-              headerLeft: () => (
-                <IconButton
-                  icon="arrow-left"
-                  onPress={() => router.replace("/home")}
-                />
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="rate"
-            options={{
-              title: "Rate",
-              headerShown: true,
-              headerLeft: () => (
-                <IconButton
-                  icon="arrow-left"
-                  onPress={() => router.replace("/profile")}
-                />
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="registration"
-            options={{
-              title: "Account Activation",
-              headerShown: true,
-              headerLeft: () => (
-                <IconButton
-                  icon="arrow-left"
-                  onPress={() => router.replace("/")}
-                />
-              ),
-            }}
-          />
-        </Stack>
-      </PaperProvider>
-    </Provider>
+            <Stack.Screen
+              name="faqs"
+              options={{
+                title: "FAQ",
+                headerShown: true,
+                headerLeft: () => (
+                  <IconButton
+                    icon="arrow-left"
+                    onPress={() => router.replace("/home")}
+                  />
+                ),
+              }}
+            />
+            <Stack.Screen
+              name="medical"
+              options={{
+                title: "Medical Records",
+                headerShown: true,
+                headerLeft: () => (
+                  <IconButton
+                    icon="arrow-left"
+                    onPress={() => router.replace("/home")}
+                  />
+                ),
+              }}
+            />
+            <Stack.Screen
+              name="bluecard"
+              options={{
+                title: "My Virtual Blue Card",
+                headerShown: true,
+                headerLeft: () => (
+                  <IconButton
+                    icon="arrow-left"
+                    onPress={() => router.replace("/profile")}
+                  />
+                ),
+              }}
+            />
+            <Stack.Screen
+              name="onlineappointment"
+              options={{
+                title: "Appointment Details",
+                headerShown: true,
+                headerLeft: () => (
+                  <IconButton
+                    icon="arrow-left"
+                    onPress={() => router.replace("/home")}
+                  />
+                ),
+              }}
+            />
+            <Stack.Screen
+              name="rate"
+              options={{
+                title: "Rate",
+                headerShown: true,
+                headerLeft: () => (
+                  <IconButton
+                    icon="arrow-left"
+                    onPress={() => router.replace("/profile")}
+                  />
+                ),
+              }}
+            />
+            <Stack.Screen
+              name="registration"
+              options={{
+                title: "Account Activation",
+                headerShown: true,
+                headerLeft: () => (
+                  <IconButton
+                    icon="arrow-left"
+                    onPress={() => router.replace("/")}
+                  />
+                ),
+              }}
+            />
+          </Stack>
+        </PaperProvider>
+      </Provider>
+    </QueryClientProvider>
     //Stack component Renders child component with page title and back button
     //Slot component Renders child component without page title and back button same as children in React
     //   <Text>Footer</Text>
