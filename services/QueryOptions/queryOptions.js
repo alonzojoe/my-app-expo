c956b32c0c9c880a56791612c5acf5398aac1d3a
alonzojoe <alonzojoenell2k15@gmail.com>
2025-07-30 15:35:01 +0800

    create new queryOptions for diagnosis receive payload PatientHistoryID and ReferID(Optional) set unique key diagnosis and the api call fetchDiagnosis for queryFn set stateTime to 30 secs

--- services/QueryOptions/queryOptions.js
+++ services/QueryOptions/queryOptions.js
@@ -1,10 +1,18 @@
 import { queryOptions } from "@tanstack/react-query";
-import { fetchPhysicians } from "../Medical/apiCalls";
+import { fetchPhysicians, fetchDiagnosis } from "../Medical/apiCalls";
 
 export const createPhysiciansQueryOptions = (PatientHistoryID, ReferID) => {
   return queryOptions({
-    queryKey: ["Physicians", PatientHistoryID, ReferID],
+    queryKey: ["physicians", PatientHistoryID, ReferID],
     queryFn: () => fetchPhysicians(PatientHistoryID, ReferID),
     staleTime: 1000 * 30, //30secs
+  });
+};
+
+export const createDiagnosisQueryOptions = (PatientHistoryID, ReferID) => {
+  return queryOptions({
+    queryKey: ["diagnosis", PatientHistoryID, ReferID],
+    queryFn: () => fetchDiagnosis(PatientHistoryID, ReferID),
+    staleTime: 1000 * 30,
   });
 };
