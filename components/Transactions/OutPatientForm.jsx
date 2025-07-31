ca0d0f98d87aa966987c064ea88c30180a4b8c58
alonzojoe <alonzojoenell2k15@gmail.com>
2025-07-31 09:38:47 +0800

    implement tanstack useQueries and implement createPhysiciansQueryOptions, and createDiagnosisQueryOptions pass the PatientHistoryID and ReferID in the queryOptions params.

--- components/Transactions/OutPatientForm.jsx
+++ components/Transactions/OutPatientForm.jsx
@@ -4,9 +4,21 @@ import { FontAwesome5 } from "@expo/vector-icons";
 import ContentTitle from "./ContentTitle";
 import ContentData from "./ContentData";
 import PhysicianItem from "./PhysicianItem";
+import { useQueries } from "@tanstack/react-query";
+import {
+  createPhysiciansQueryOptions,
+  createDiagnosisQueryOptions,
+} from "./../../services/QueryOptions/queryOptions";
 
-const OutPatientForm = ({ selected, physicians, soap, onToggle }) => {
-  const { TransactionNo } = selected;
+const OutPatientForm = ({ selected, onToggle }) => {
+  const { TransactionNo, PatientHistoryID, ReferID } = selected;
+
+  const [physicianss, diagnostics] = useQueries({
+    queries: [
+      createPhysiciansQueryOptions(PatientHistoryID, ReferID),
+      createDiagnosisQueryOptions(PatientHistoryID, ReferID),
+    ],
+  });
 
   return (
     <>
@@ -27,7 +39,7 @@ const OutPatientForm = ({ selected, physicians, soap, onToggle }) => {
             fontWeight: "bold",
           }}
         >
-          {TransactionNo}
+          {TransactionNo} {PatientHistoryID} {ReferID}
         </PaperText>
         <PaperText onPress={() => onToggle(false)}>
           <FontAwesome5 name="times" size={20} color="#DD3254" />
@@ -38,17 +50,17 @@ const OutPatientForm = ({ selected, physicians, soap, onToggle }) => {
           <>
             <ContentTitle title="Physicians" mb={5} />
             <View style={{ marginBottom: 5 }}>
-              {physicians.map((p) => (
+              {/* {physicians.map((p) => (
                 <PhysicianItem
                   key={p.id}
                   physician={p.name}
                   type={p.type}
                   isMain={p.isMain}
                 />
-              ))}
+              ))} */}
             </View>
           </>
-          <>
+          {/* <>
             <ContentTitle title="SOAP" />
             <ContentData title={`Subjective`} content={soap.Subjective} />
             <ContentData title={`Objective`} content={soap.Objective} />
@@ -56,7 +68,7 @@ const OutPatientForm = ({ selected, physicians, soap, onToggle }) => {
           <>
             <ContentData title={`Assessment`} content={soap.Assessment} />
             <ContentData title={`Plan`} content={soap.Plan} />
-          </>
+          </> */}
         </View>
         <View
           style={{
