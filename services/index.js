e56c991a107bac67a62d20aef713d476ddec7aee
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-05 17:00:38 +0800

    remove apiopd in index make a separate file for it due to export issues

--- services/index.js
+++ services/index.js
@@ -4,8 +4,4 @@ const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
 });
 
-export const apiopd = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_OPD,
-});
-
 export default api;
