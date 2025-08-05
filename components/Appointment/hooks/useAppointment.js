72c3288e28d0b18cc1a5448d10b1240bd7e4b07c
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-05 11:38:40 +0800

    create new api call getDateSlots in useAppointment custom hook  using the apiopd service and set availableDates in resolve state of the api call.

--- components/Appointment/hooks/useAppointment.js
+++ components/Appointment/hooks/useAppointment.js
@@ -1,9 +1,19 @@
 import { useState } from "react";
-import api from "../../../services";
+import { apiopd } from "../../../services";
 const useAppointment = () => {
   const [availableDates, setAvailableDates] = useState([]);
 
-  const getDateSlots = async () => {};
+  const getDateSlots = async () => {
+    try {
+      const res = await apiopd.get("/date?ServiceType=212");
+      console.log("ddddddddd", res.data);
+      setAvailableDates(res.data);
+    } catch (error) {
+      console.log("error");
+    }
+  };
+
+  return { availableDates, getDateSlots };
 };
 
 export default useAppointment;
