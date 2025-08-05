2b5b586724614a8723016d4c5f4292cf9bd8e0c6
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-05 11:14:47 +0800

    create new service apiopd set the baseURL to EXPO_PUBLIC_API_OPD from .env file

--- services/index.js
+++ services/index.js
@@ -4,4 +4,8 @@ const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
 });
 
+export const apiopd = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_OPD,
+});
+
 export default api;
