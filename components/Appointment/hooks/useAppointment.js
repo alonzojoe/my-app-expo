0f59303169706860d2ee46a246a3cec10efc8b65
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-05 11:08:04 +0800

    create new custom hook useAppointment temporary declare availableDates state with empty array initial value

---
+++ components/Appointment/hooks/useAppointment.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import api from "../../../services";
+const useAppointment = () => {
+  const [availableDates, setAvailableDates] = useState([]);
+
+  const getDateSlots = async () => {};
+};
+
+export default useAppointment;
