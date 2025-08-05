fef508938a7e1594986f11bab762b830c85225c3
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-05 09:58:34 +0800

    modify useNetInfo custom hook add router to redirect in /nointernet path when hasNet state is falsy.

--- hooks/useNetInfo.js
+++ hooks/useNetInfo.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import NetInfo from "@react-native-community/netinfo";
+import { useRouter } from "expo-router";
 
 const useNetInfo = () => {
   const [hasNet, setHasNet] = useState(true);
+
+  const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener((state) => {
@@ -11,6 +14,11 @@ const useNetInfo = () => {
 
     return () => unsubscribe();
   });
+
+  if (!hasNet) {
+    router.replace("/nointernet");
+    return null;
+  }
 
   return hasNet;
 };
