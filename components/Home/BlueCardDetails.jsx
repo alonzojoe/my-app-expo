17a26e0967091129a2e83b0eafc4dcdf626cfa6d
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-07 14:55:01 +0800

    implement QRCode component in BlueCardDetails styled and wrap it with overlayContainer add static value for the RCode size set to 80

--- components/Home/BlueCardDetails.jsx
+++ components/Home/BlueCardDetails.jsx
@@ -1,12 +1,16 @@
 import { StyleSheet, View, Image, Text } from "react-native";
 import React from "react";
 import BlueF from "../../assets/image/bluef.jpg";
+import QRCode from "react-native-qrcode-svg";
 
 const BlueCardDetails = () => {
   return (
     <View style={styles.imgcontainer}>
       <View style={styles.cardContainer}>
         <Image source={BlueF} style={styles.cardImage} />
+        <View style={styles.overlayContainer}>
+          <QRCode value="01352296" size={80} />
+        </View>
         <Text style={styles.patientno}>01352296</Text>
         <Text style={styles.name}>DEANG, DELIA DIZON</Text>
         <Text style={styles.bday}>March 14, 1938</Text>
@@ -45,16 +49,15 @@ const styles = StyleSheet.create({
     height: "100%",
     resizeMode: "cover",
   },
-  // overlayView: {
-  //   position: "absolute",
-  //   bottom: 60,
-  //   left: 65,
-  //   right: 10,
-  //   backgroundColor: "rgba(0, 0, 0, 0.5)",
-  //   // backgroundColor: "transparent",
-  //   padding: 10,
-  //   borderRadius: 8,
-  // },
+  overlayContainer: {
+    position: "absolute",
+    top: 7,
+    right: 7,
+    zIndex: 10,
+    backgroundColor: "white", // optional: add contrast
+    padding: 4,
+    borderRadius: 4,
+  },
   name: {
     position: "absolute",
     bottom: 93,
@@ -110,5 +113,11 @@ const styles = StyleSheet.create({
     color: "#FFF",
     fontSize: 24,
     fontWeight: "bold",
+  },
+  qr: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    zIndex: 99999,
   },
 });
