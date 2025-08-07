bc4c6301aa2569c87e7e699bff35dbed38fb29a7
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-07 14:03:00 +0800

    styled BlueCardDetails add text overlay for name, birthdate, address, gender, civil status and date issued

--- components/Home/BlueCardDetails.jsx
+++ components/Home/BlueCardDetails.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
 import React from "react";
 import BlueF from "../../assets/image/bluef.jpg";
 
@@ -7,6 +7,15 @@ const BlueCardDetails = () => {
     <View style={styles.imgcontainer}>
       <View style={styles.cardContainer}>
         <Image source={BlueF} style={styles.cardImage} />
+
+        <Text style={styles.name}>DEANG, DELIA DIZON</Text>
+        <Text style={styles.bday}>March 14, 1938</Text>
+        <Text style={styles.address}>
+          501 DOLORES, CITY OF SAN FERNANDO PAMPANGA
+        </Text>
+        <Text style={styles.gender}>FEMALE</Text>
+        <Text style={styles.civil}>MARRIED</Text>
+        <Text style={styles.issued}>-</Text>
       </View>
     </View>
   );
@@ -19,6 +28,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   cardContainer: {
+    position: "relative",
     width: "90%",
     aspectRatio: 1.6,
     alignSelf: "center",
@@ -34,5 +44,63 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     resizeMode: "cover",
+  },
+  // overlayView: {
+  //   position: "absolute",
+  //   bottom: 60,
+  //   left: 65,
+  //   right: 10,
+  //   backgroundColor: "rgba(0, 0, 0, 0.5)",
+  //   // backgroundColor: "transparent",
+  //   padding: 10,
+  //   borderRadius: 8,
+  // },
+  name: {
+    position: "absolute",
+    bottom: 93,
+    left: 70,
+    color: "black",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  bday: {
+    position: "absolute",
+    bottom: 74,
+    left: 70,
+    color: "black",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  address: {
+    position: "absolute",
+    bottom: 60,
+    left: 70,
+    color: "black",
+    fontSize: 10,
+    fontWeight: "bold",
+  },
+  gender: {
+    position: "absolute",
+    bottom: 40,
+    left: 70,
+    color: "black",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  civil: {
+    position: "absolute",
+    bottom: 21,
+    left: 70,
+    color: "black",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  issued: {
+    position: "absolute",
+    bottom: 4,
+    left: 70,
+    color: "black",
+    fontSize: 15,
+    fontWeight: "bold",
   },
 });
