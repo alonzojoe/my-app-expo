b2b8bdc305c67a5d32cbf1ff83757ecc93f193ad
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-13 10:47:07 +0800

    extract info from authUser (logged user) from store in useBluecardInfo, memoized civilstatus label and return the PatientInfo from the hook

--- hooks/features/useBluecardInfo.js
+++ hooks/features/useBluecardInfo.js
@@ -1,8 +1,26 @@
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CIVIL_STATUS } from "../../constants/global";
+import moment from "moment";
 
 const useBluecardInfo = () => {
   const { authUser } = useSelector((state) => state.auth);
+
+  const label = useMemo(
+    () => CIVIL_STATUS.find((c) => c.id === authUser?.CivilStatusID),
+    [authUser]
+  );
+
+  const PatientInfo = {
+    name: authUser?.FullName,
+    birthdate: moment(authUser?.BirthDate).format("MMMM DD, YYYY"),
+    address: "-",
+    gender: authUser?.Gender === "F" ? "FEMALE" : "MALE",
+    civilStatus: label,
+    dateissued: "-",
+  };
+
+  return PatientInfo;
 };
 
 export default useBluecardInfo;
