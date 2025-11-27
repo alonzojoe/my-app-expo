34a0fe00b08a9964bbd994a11568204a3b8dd86e
alonzojoe <alonzojoenell2k15@gmail.com>
2025-11-27 14:28:06 +0800

    refactor FormData create new custom hook useOnlineAppointment and move all logic in the hook (separate UI from logic)

--- components/Appointment/components/FormData.jsx
+++ components/Appointment/components/FormData.jsx
@@ -1,88 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { useRouter } from "expo-router";
 import {
   Text as PaperText,
   TextInput,
   Button,
   ActivityIndicator,
 } from "react-native-paper";
-import { useForm, Controller } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import ContentTitle from "./../../Transactions/ContentTitle";
-import { eappointmentForm } from "../../../schema/schema";
-import { useSelector } from "react-redux";
-import {
-  checkSlots,
-  createOnlineAppointment,
-} from "../../../services/Medical/apiCalls";
+import useOnlineAppointment from "../hooks/useOnlineAppointment";
 
 const FormData = ({ data }) => {
+  const { Controller, control, handleSubmit, errors, isSubmitting, onSubmit } =
+    useOnlineAppointment();
+
   console.log("data", data);
-  const { authUser } = useSelector((state) => state.auth);
-
-  const router = useRouter();
-
-  const defaultValues = {
-    phone: "",
-    complaints: "",
-  };
-
-  const {
-    control,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-    reset,
-  } = useForm({
-    defaultValues,
-    resolver: zodResolver(eappointmentForm),
-  });
-
-  const onSubmit = async (formData) => {
-    console.log("auth", authUser);
-    console.log("selected slot", data);
-    console.log("Form Data:", formData);
-
-    const slotPayload = {
-      serviceId: data?.serviceId,
-      opdtimeid: data?.selectedSlot?.opdtimeid,
-      date: data?.date,
-    };
-
-    const res = await checkSlots(slotPayload);
-
-    console.log("checkSlots", res.length);
-
-    if (!res || res.length === 0) {
-      alert("Please select another slot!");
-      return;
-    }
-
-    const payload = {
-      ...authUser,
-      Barangay: authUser?.BarangayID,
-      BirthDate: authUser?.birthdate,
-      OldNew: 0,
-      ChiefComplaint: formData?.complaints?.toUpperCase(),
-      AltContactNo: formData?.phone,
-      SelectedTime: data?.selectedSlot,
-      SelectedDate: data?.selectedSlot?.date,
-      SelectedDateId: data?.selectedSlot?.opddateslotsid,
-      ServiceType: data?.serviceId,
-    };
-
-    console.log("updated payload", payload);
-
-    const createApt = await createOnlineAppointment(payload);
-
-    console.log("create apt", createApt);
-    alert("Appointment created successfully!");
-    router.replace("/schedule");
-    return;
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    // reset();
-  };
 
   return (
     <View style={styles.container}>
